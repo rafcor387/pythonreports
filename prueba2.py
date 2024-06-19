ef228c07cfbf9578e23ae0f73157533ceb12a8ec
rafcor387 <144506230+rafcor387@users.noreply.github.com>
2024-06-19 14:29:59 -0400

    Update prueba2.py
    cosa

--- prueba2.py
+++ prueba2.py
@@ -1,9 +1,37 @@
-import streamlit as st
 import pandas as pd
+import streamlit as st
+from PIL import Image
 
-@st.cache(allow_output_mutation=True)
-def load_data():
-    df = pd.read_csv('https://raw.githubusercontent.com/rafcor387/pythonreports/main/covid_africa.csv')
-    return df
-df = load_data()
-st.write(df)
+st.header('Gráficas utilizando Pandas', divider='rainbow')
+st.title("Resultados del Grand Prix de Países Bajos")
+
+image = Image.open('Verstappen-pole-lap-Zandvoort-Netherlands-2021.jpg')
+st.image(image, caption='Max Verstappen')
+
+
+st.text_input("¿Cuál es tu nombre?", key="name")
+st.session_state.name
+
+st.text('¡Hola '+st.session_state.name+' !') 
+'Hola cómo estás? ',st.session_state.name
+
+df = pd.read_csv('https://raw.githubusercontent.com/LilianaC/streamlit3/main/Datos%20F1%20Dutch%20GP%20-%20Sheet1.csv')
+
+if st.checkbox('Mostrar dataframe'):
+    df
+
+
+option = st.selectbox(
+    'Selecciona tu corredor favorito: ',
+     df['DRIVER'])
+
+'Tu selección: ', option
+
+df.loc[df['DRIVER'] == option]
+
+
+st.line_chart(
+    df,
+    x = 'AVG SPEED',
+    y = 'LAP'
+)
