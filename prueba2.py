45cd402ffbe158ff90656edcb0859c43bbc37e8d
rafco <rafco@LOBO>
2024-06-19 15:03:10 -0400

    torta

--- prueba2.py
+++ prueba2.py
@@ -30,3 +30,8 @@ st.bar_chart(
 
 st.text('Mapa de los países') 
 st.map(df[['latitude', 'longitude']])
+
+
+st.text('Gráfico de torta de Total Cases por país') 
+total_cases_by_country = df.groupby('Country/Other')['Total Cases'].sum()
+st.pie_chart(total_cases_by_country)
