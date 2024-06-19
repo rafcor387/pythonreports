9d7d096a04555c85f9442de66e6dec43698de7dc
rafcor387 <144506230+rafcor387@users.noreply.github.com>
2024-06-19 14:25:20 -0400

    Update prueba2.py
    dsf

--- prueba2.py
+++ prueba2.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 @st.cache(allow_output_mutation=True)
 def load_data():
-    df = pd.read_csv('covid_africa.csv')
+    df = pd.read_csv('https://raw.githubusercontent.com/rafcor387/pythonreports/main/covid_africa.csv')
     return df
 df = load_data()
 st.write(df)
