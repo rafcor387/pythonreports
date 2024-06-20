8889cd204bd063bcc1870877b78a98e464aca4ad
rafco <rafco@LOBO>
2024-06-20 19:28:36 -0400

    con disco y fecha

--- prueba2.py
+++ prueba2.py
@@ -24,12 +24,13 @@ df.loc[df['cliente'] == option]
 default_date = datetime(2023, 1, 1)
 # Campo de texto para filtrar por fecha con fecha por defecto
 date_input = st.date_input('Selecciona una fecha:', default_date)
+# Campo de selección para elegir entre Forum, Pacha, Zouk Boulevard
+selected_location = st.selectbox('Selecciona una ubicación:', ['Forum', 'Pacha', 'Zouk Boulevard'])
 
 # Convierte la columna 'fechaHora' al tipo de dato de fecha y hora
 df['fechaHora'] = pd.to_datetime(df['fechaHora'])
+# Filtra los registros que coinciden con la fecha seleccionada y la ubicación seleccionada
+filtered_data = df[(df['fechaHora'].dt.date == date_input) & (df['nombre'] == selected_location)]
 
-# Filtra los registros que coinciden con la fecha seleccionada
-filtered_data = df[df['fechaHora'].dt.date == date_input]
-
-# Muestra el número de registros con esa fecha
-st.text(f'El número de registros con esa fecha es: {len(filtered_data)}')
+# Muestra el número de registros con esa fecha y ubicación iguales
+st.text(f'El número de registros con esa fecha y ubicación iguales es: {len(filtered_data)}')
