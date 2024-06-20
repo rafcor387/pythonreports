73275e08f92d3f97b0a4db41f26cfe61a8e8ca3e
rafco <rafco@LOBO>
2024-06-20 19:37:27 -0400

    haber

--- prueba2.py
+++ prueba2.py
@@ -24,30 +24,24 @@ df.loc[df['cliente'] == option]
 default_date = datetime(2023, 1, 1)
 # Campo de texto para filtrar por fecha con fecha por defecto
 date_input = st.date_input('Selecciona una fecha:', default_date)
-# Campo de selección para elegir entre Forum, Pacha, Zouk Boulevard
-selected_location = st.selectbox('Selecciona una ubicación:', ['Forum', 'Pacha', 'Zouk Boulevard'])
 
 # Convierte la columna 'fechaHora' al tipo de dato de fecha y hora
 df['fechaHora'] = pd.to_datetime(df['fechaHora'])
-# Filtra los registros que coinciden con la fecha seleccionada y la ubicación seleccionada
-filtered_data = df[(df['fechaHora'].dt.date == date_input) & (df['nombre'] == selected_location)]
 
-# Muestra el número de registros con esa fecha y ubicación iguales
-st.text(f'El número de registros con esa fecha y ubicación iguales es: {len(filtered_data)}')
+# Filtra los registros que coinciden con la fecha seleccionada
+filtered_data = df[df['fechaHora'].dt.date == date_input]
 
-# Calcula el porcentaje de cada nombre con respecto al total de filas
+# Calcula el total de registros para cada nombre
 nombre_counts = filtered_data['nombre'].value_counts()
-percentage = (nombre_counts / len(filtered_data)) * 100
 
 # Crea un gráfico de pastel usando Matplotlib
 fig, ax = plt.subplots()
 ax.pie(nombre_counts, labels=nombre_counts.index, autopct='%1.1f%%', startangle=90)
 ax.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
-ax.set_title(f'Distribución de nombres para {selected_location} en la fecha seleccionada')
+ax.set_title(f'Distribución de nombres para todas las ubicaciones en la fecha seleccionada')
 
 # Muestra el gráfico de pastel
 st.pyplot(fig)
 
-# Muestra el porcentaje de cada nombre
-st.write("Porcentaje de cada nombre:")
-st.write(percentage)
+# Campo de selección para elegir entre Forum, Pacha, Zouk Boulevard
+#selected_location = st.selectbox('Selecciona una ubicación:', ['Forum', 'Pacha', 'Zouk Boulevard'])
