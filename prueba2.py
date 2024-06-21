8b23361e2e3565e9cccadedfcb27ac2901709703
rafco <rafco@LOBO>
2024-06-21 16:22:59 -0400

    dsf

--- prueba2.py
+++ prueba2.py
@@ -80,9 +80,7 @@ ax.set_ylabel('Número de reservas')
 ax.set_title(f'Número de reservas realizadas por dia en {selected_month} para {selected_name}')
 # Muestra el gráfico de líneas
 st.pyplot(fig)
-# Muestra el DataFrame filtrado (opcional)
-if st.checkbox('Mostrar dataframe'):
-    st.write(filtered_data)
+
 
 
 # -------------------------------
